01f3c4c50327bba49056278f9c32db0cf18d5740
Santiago Almeida Bolaños <santiagodeveloper666@gmail.com>
2018-09-25 18:48:49 -0400

    upadting

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# santiagoalmeidabolannos.github.io
+# Santiago Almeida Bolaños
+## Personal website
+
+A simple website using just Bootstrap 4 and some JQuery plugins for improve performance. I hope you found everything you need to know about me. I am open for remote jobs, Thanks for reading 😊
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,70 @@
-$(document).ready(function(){
-    console.log('ready');
-});
+function sendMessage() {
+    console.log('hello world');
+}
 
-var constraints = {
-    email: {
-        email: true,
-        presence:true
+function validateForm() {
+    var valid = true;
+    $('#name').html(function () {
+        if(!$(this)[0].validity.valid) {
+            var message = $(this)[0].validationMessage;
+            $('#nameHelpBlock').html(function () {
+                $(this)[0].innerHTML = message;
+            });
+            valid = false;
+        }
+    });
+    $('#email').html(function () {
+        if(!$(this)[0].validity.valid) {
+            var message = $(this)[0].validationMessage;
+            $('#emailHelpBlock').html(function () {
+                $(this)[0].innerHTML = message;
+            });
+            valid = false;
+        }
+    });
+    $('#message').html(function () {
+        if(!$(this)[0].validity.valid) {
+            var message = $(this)[0].validationMessage;
+            $('#messageHelpBlock').html(function () {
+                $(this)[0].innerHTML = message;
+            });
+            valid = false;
+        }
+    });
+    return valid;
+}
+
+function cleanErrors() {
+    $('#messageHelpBlock').html(function () {
+        $(this)[0].innerHTML = '';
+    });
+    $('#emailHelpBlock').html(function () {
+        $(this)[0].innerHTML = '';
+    });
+    $('#nameHelpBlock').html(function () {
+        $(this)[0].innerHTML = '';
+    });
+}
+
+$('#send_button').click(function () {
+
+    cleanErrors();
+    // sendMessage();
+    if(validateForm()) {
+        var data = {};
+        $('#name').text(function () {
+            data.name = $(this)[0].value;
+        });
+        $('#email').text(function () {
+            data.email = $(this)[0].value;
+        });
+        $('#message').text(function () {
+            data.message = $(this)[0].value;
+        });
+        $.post('http://localhost:3000/message', data, function (err, info) {
+           alert('message sended');
+           console.log(err);
+           console.log(info);
+        });
     }
-};
+});
