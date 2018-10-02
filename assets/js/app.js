1de9018c61c275c5cc354fefc755015cf23c4bcd
Santiago Almeida Bolaños <santiagodeveloper666@gmail.com>
2018-10-02 12:38:35 -0400

    adding manifest

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,66 +1,43 @@
-function sendMessage() {
-    console.log('hello world');
-}
-
 function validateForm() {
     var valid = true;
-    $('#name').html(function () {
-        if(!$(this)[0].validity.valid) {
-            var message = $(this)[0].validationMessage;
-            $('#nameHelpBlock').html(function () {
-                $(this)[0].innerHTML = message;
-            });
-            valid = false;
-        }
-    });
-    $('#email').html(function () {
-        if(!$(this)[0].validity.valid) {
-            var message = $(this)[0].validationMessage;
-            $('#emailHelpBlock').html(function () {
-                $(this)[0].innerHTML = message;
-            });
-            valid = false;
-        }
-    });
-    $('#message').html(function () {
-        if(!$(this)[0].validity.valid) {
-            var message = $(this)[0].validationMessage;
-            $('#messageHelpBlock').html(function () {
-                $(this)[0].innerHTML = message;
-            });
-            valid = false;
-        }
-    });
+
+    var $name = $('#name');
+    var $email = $('#email');
+    var $message = $('#message');
+
+    if(!$name.prop('validity').valid) {
+        $('#nameHelpBlock').html($name.prop('validationMessage'));
+        valid = false;
+    }
+    if(!$email.prop('validity').valid) {
+        $('#emailHelpBlock').html($email.prop('validationMessage'));
+        valid = false;
+    }
+    if(!$message.prop('validity').valid) {
+        $('#messageHelpBlock').html($message.prop('validationMessage'));
+        valid = false;
+    }
+
     return valid;
 }
 
 function cleanErrors() {
-    $('#messageHelpBlock').html(function () {
-        $(this)[0].innerHTML = '';
-    });
-    $('#emailHelpBlock').html(function () {
-        $(this)[0].innerHTML = '';
-    });
-    $('#nameHelpBlock').html(function () {
-        $(this)[0].innerHTML = '';
-    });
+    $('#messageHelpBlock').html('');
+    $('#emailHelpBlock').html('');
+    $('#nameHelpBlock').html('');
 }
 
-$('#send_button').click(function () {
-
+$('#send_button').click(function (ev) {
+    ev.preventDefault();
     cleanErrors();
-    // sendMessage();
+
     if(validateForm()) {
-        var data = {};
-        $('#name').text(function () {
-            data.name = $(this)[0].value;
-        });
-        $('#email').text(function () {
-            data.email = $(this)[0].value;
-        });
-        $('#message').text(function () {
-            data.message = $(this)[0].value;
-        });
+        var data = {
+            name: $('#name').val(),
+            email: $('#email').val(),
+            message: $('#message').val()
+        };
+        console.log(data);
         $.post('http://localhost:3000/message', data, function (err, info) {
            alert('message sended');
            console.log(err);
