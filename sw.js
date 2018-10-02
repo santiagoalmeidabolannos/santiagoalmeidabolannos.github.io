e6f3555b64b4a31f81b888ad15c167592b706c14
Santiago Almeida Bolaños <santiagodeveloper666@gmail.com>
2018-10-02 13:18:59 -0400

    updating service-worker

--- sw.js
+++ sw.js
@@ -1,19 +1,29 @@
-var CACHE_NAME = 'my-site-cache-v1';
-var urlsToCache = [
+// Copyright 2016 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+var dataCacheName = 'santiagodeveloperData-v1';
+var cacheName = 'santiagodeveloper-v1';
+var filesToCache = [
     '/',
+    '/index.html',
     '/assets/css/animate.css',
     '/assets/css/app.css',
     '/assets/css/bootstrap.css',
-    '/assets/css/bootstrap-grid.css',
-    '/assets/css/bootstrap-reboot.css',
     '/assets/css/fa-brands.css',
-    '/assets/css/fa-light.css',
-    '/assets/css/fa-regular.css',
-    '/assets/css/fa-solid.css',
-    '/assets/css/fontawesome.css',
     '/assets/css/fontawesome-all.css',
     '/assets/js/app.js',
-    '/assets/js/bootstrap.bundle.js',
     '/assets/js/bootstrap.min.js',
     '/assets/js/jquery.min.js',
     '/assets/js/navbar-fixed.js',
@@ -22,44 +32,51 @@ var urlsToCache = [
     '/assets/img/profile_square.jpg'
 ];
 
-self.addEventListener('install', function(event) {
-    // Perform install steps
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(function(cache) {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
+self.addEventListener('install', function (e) {
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
     );
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request)
-            .then(function(response) {
-                    // Cache hit - return response
-                    if (response) {
-                        return response;
-                    }
-                    return fetch(event.request);
+self.addEventListener('activate', function (e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            return Promise.all(keyList.map(function (key) {
+                if (key !== cacheName && key !== dataCacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
                 }
-            )
+            }));
+        })
     );
+    /*
+     * Fixes a corner case in which the app wasn't returning the latest data.
+     * You can reproduce the corner case by commenting out the line below and
+     * then doing the following steps: 1) load app for first time so that the
+     * initial New York City data is shown 2) press the refresh button on the
+     * app 3) go offline 4) reload the app. You expect to see the newer NYC
+     * data, but you actually see the initial data. This happens because the
+     * service worker is not yet activated. The code below essentially lets
+     * you activate the service worker faster.
+     */
+    return self.clients.claim();
 });
 
-self.addEventListener('activate', function(event) {
-
-    var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
-
-    event.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(
-                cacheNames.map(function(cacheName) {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
+self.addEventListener('fetch', function (e) {
+    console.log('[Service Worker] Fetch', e.request.url);
+    /*
+     * The app is asking for app shell files. In this scenario the app uses the
+     * "Cache, falling back to the network" offline strategy:
+     * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
+     */
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
         })
     );
 });
